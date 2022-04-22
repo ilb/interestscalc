25e89470496ba9ab6610c67b96b815ec3796adb2
Vjacheslav Borisov <slavb@ilb.ru>
2022-04-22 11:06:54 +0400

    преобразования map и дат

--- src/InterestsCalc.js
+++ src/InterestsCalc.js
@@ -1,6 +1,9 @@
 export default class InterestsCalc {
   /**
-   * Расчет процентов за период
+   * Расчет процентов за период.
+   * Входные массивы remainers и rates можно задавать как Map,
+   * так и как обычный объект {'2022-04-01': 0.19, '2022-04-15': 0.3}. Ключем может быть
+   * дата или строка формата YYYY-MM-DD
    * @param {Date} begDate дата начала периода
    * @param {Date} endDate дата окончания периода
    * @param {Map} remainders остатки (дата->остаток)
@@ -8,8 +11,34 @@ export default class InterestsCalc {
    */
   calcInterests(begDate, endDate, remainders, rates) {
     [begDate, endDate] = checkPeriod(begDate, endDate);
-    console.log({ begDate, endDate });
+    remainders = convertMap(remainders);
+    rates = convertMap(rates);
+    console.log({ begDate, endDate, remainders, rates });
   }
+}
+
+/**
+ * Преобразование объекта в Map (дата => значение)
+ * @param {*} date
+ * @returns Map
+ */
+export function convertMap(map) {
+  if (!(map instanceof Map)) {
+    map = objToMap(map, convertDate);
+  }
+  return map;
+}
+
+/**
+ * Преобразование даты из строки YYYY-MM-DD в дату
+ * @param {*} date
+ * @returns Date
+ */
+export function convertDate(date) {
+  if (!(date instanceof Date)) {
+    date = new Date(date);
+  }
+  return date;
 }
 
 /**
@@ -18,14 +47,48 @@ export default class InterestsCalc {
  * @param {Date} endDate дата окончания периода
  */
 export function checkPeriod(begDate, endDate) {
-  if (!(begDate instanceof Date)) {
-    begDate = new Date(begDate);
-  }
-  if (!(endDate instanceof Date)) {
-    endDate = new Date(endDate);
-  }
+  begDate = convertDate(begDate);
+  endDate = convertDate(endDate);
   if (begDate > endDate) {
     throw new Error(`begDate if after endDate: ${begDate} > ${endDate}`);
   }
   return [begDate, endDate];
+}
+
+/**
+ * Чистка массива по периоду: если дата значения
+ * @param {Date} begDate дата начала периода
+ * @param {Date} endDate дата окончания периода
+ * @param {Map} map значения (дата->значение), отсортированные по дате
+ */
+export function filterMap(begDate, endDate, map) {
+  [begDate, endDate] = checkPeriod(begDate, endDate);
+  map = convertMap(map);
+
+  const result = new Map();
+  map.forEach((value, date) => {
+    date = convertDate(date);
+    if (date < endDate) {
+      result.set(date < begDate ? begDate : date, value);
+    }
+  });
+  return result;
+}
+
+/**
+ * Преобразование объекта в map
+ * @param {*} obj входной объект
+ * @param {function} keyMapper опциональная функция преобразования ключа
+ * @param {*} valueMapper опциональная функция преобразования значения
+ * @returns Map
+ */
+export function objToMap(obj, keyMapper, valueMapper) {
+  const map = new Map();
+  keyMapper = keyMapper || ((x) => x);
+  valueMapper = valueMapper || ((x) => x);
+
+  for (let k of Object.keys(obj)) {
+    map.set(keyMapper(k), valueMapper(obj[k]));
+  }
+  return map;
 }
--- test/InterestsCalc.test.js
+++ test/InterestsCalc.test.js
@@ -1,12 +1,17 @@
-import { default as InterestsCalc, checkPeriod } from '../src/InterestsCalc';
+import InterestsCalc, { checkPeriod, filterMap } from '../src/InterestsCalc';
 
 function testSet1() {
   const begDate = new Date('2022-04-01');
   const endDate = new Date('2022-04-30');
 
-  const rates = new Map();
-  rates.set(new Date('2022-04-01'), 0.19);
-  rates.set(new Date('2022-04-15'), 0.3);
+  // const rates = new Map();
+  // rates.set(new Date('2022-04-01'), 0.19);
+  // rates.set(new Date('2022-04-15'), 0.3);
+
+  const rates = {
+    '2022-04-01': 0.19,
+    '2022-04-15': 0.3
+  };
   const remainders = new Map();
   remainders.set(new Date('2022-04-01'), 100000);
   remainders.set(new Date('2022-04-15'), 50000);
@@ -30,4 +35,19 @@ test('checkPeriod', () => {
   expect(() => {
     checkPeriod('2022-05-01', '2022-04-30');
   }).toThrow('begDate if after endDate');
+});
+
+test('filterMap', () => {
+  const map = new Map();
+  map.set(new Date('2022-03-01'), 0.1);
+  map.set(new Date('2022-04-03'), 0.19);
+  map.set(new Date('2022-04-15'), 0.3);
+  map.set(new Date('2022-05-15'), 0.35);
+  const expected = new Map();
+  expected.set(new Date('2022-04-01'), 0.1);
+  expected.set(new Date('2022-04-03'), 0.19);
+  expected.set(new Date('2022-04-15'), 0.3);
+
+  const result = filterMap('2022-04-01', '2022-04-30', map);
+  expect(result).toEqual(expected);
 });
